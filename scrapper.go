93d9ea0289ef24ae3c4283a017a6a74d6c052f55
cotabas <cotabas@gmail.com>
2024-09-12 22:08:18 -0700

    pulls recipes and saves them as json

--- scrapper.go
+++ scrapper.go
@@ -8,25 +8,38 @@ import (
 )
 
 func main() {
-  getFirstPage()
+  //get the links to all the recipes
+  //getLinx(getFirstPage())
+  inFile, _ := os.ReadFile("output.txt")
+  linx := strings.Split(string(inFile), "\n")
+
+  for i, line := range linx[:len(linx) - 1] {
+    curl := exec.Command("curl", line)
+    out, err := curl.Output()
+    if err != nil { fmt.Println("curl output error" , err) }
+
+    recipe := strings.Split(string(out), `<script type="application/ld+json">`)[1]
+    recipe = strings.Split(recipe, `</script>`)[0]
+
+    outFile, err := os.Create("../recipes/ms" + fmt.Sprint(i) + ".json")
+    if err != nil { fmt.Println("file open error" , err) }
+    outFile.WriteString(recipe)
+    outFile.Sync()
+
+    fmt.Println(i, line)
+  }
 }
 
-func getLinx() {
-  linx := []string{}
+func getLinx(linx []string) {
   outFile, err := os.Create("output.txt")
-  if err != nil {
-    fmt.Println(" file open error" , err)
-  }
+  if err != nil { fmt.Println("file open error" , err) }
 
   for i := 2; i < 181; i++ {
     recipeLink := "https://www.177milkstreet.com/recipes/p" + fmt.Sprint(i)
-    fmt.Println(i)
     fmt.Println(recipeLink)
     curl := exec.Command("curl", recipeLink) 
     out, err := curl.Output()
-    if err != nil {
-      fmt.Println("curl output error" , err)
-    }
+    if err != nil { fmt.Println("curl output error" , err) }
 
     lines := strings.Split(string(out), "\n")
     for _, line := range lines {
@@ -43,15 +56,13 @@ func getLinx() {
   }
 }
 
-func getFirstPage() {
+func getFirstPage() []string {
   linx := []string{}
 
   recipeLink := "https://www.177milkstreet.com/recipes"
   curl := exec.Command("curl", recipeLink) 
   out, err := curl.Output()
-  if err != nil {
-    fmt.Println("curl output error" , err)
-  }
+  if err != nil { fmt.Println("curl output error" , err) }
 
   lines := strings.Split(string(out), "\n")
   for _, line := range lines {
@@ -63,4 +74,6 @@ func getFirstPage() {
   for _, line := range linx {
     fmt.Println(line)
   }
+
+  return linx
 }
